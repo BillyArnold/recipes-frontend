c8b34e58aac0374e4631b488f77dc6ec4b367b60
Billy Arnold <billyarnoldd@gmail.com>
2024-02-01 19:44:32 +0000

    Adding mealplan selector to mealplan drawer

--- components/buttons/AddToMealPlanButton.tsx
+++ components/buttons/AddToMealPlanButton.tsx
@@ -15,6 +15,14 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectValue,
+  SelectItem,
+} from "@/components/ui/select";
 import getSavedRecipesForUser from "@/app/actions/getSavedRecipesForUser";
 
 interface SaveRecipeButtonProps {
@@ -23,14 +31,16 @@ interface SaveRecipeButtonProps {
 export default function AddToMealPlanButton({
   recipeId,
 }: SaveRecipeButtonProps) {
-  const [mealPlans, setMealPlans] = useState(null);
+  const [mealPlans, setMealPlans] = useState([]);
   const [loadingMealPlans, setLoadingMealPlans] = useState(true);
+  const [selectedMealPlan, setSelectedMealPlan] = useState<any>();
   const auth = useAuth();
 
   useEffect(() => {
     if (auth.user?.id) {
       const res = getSavedRecipesForUser(auth.user).then((data) => {
         setMealPlans(data.mealPlans);
+        setSelectedMealPlan(data.mealPlans[0]);
         setLoadingMealPlans(false);
       });
     } else {
@@ -49,16 +59,33 @@ export default function AddToMealPlanButton({
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>Which Meal Plan to add to?</DrawerTitle>
+          <DrawerTitle className="mb-4">Which Meal Plan to add to?</DrawerTitle>
           <DrawerDescription>
             {loadingMealPlans ? (
               <div>Loading meal plans...</div>
             ) : (
-              <div>Meals loaded</div>
+              <Select
+                value={selectedMealPlan?.id}
+                onValueChange={(value) => console.log(value)}
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {mealPlans &&
+                      mealPlans.map((mealPlan: any) => (
+                        <SelectItem key={mealPlan.id} value={mealPlan.id}>
+                          {mealPlan.name}
+                        </SelectItem>
+                      ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             )}
           </DrawerDescription>
         </DrawerHeader>
-        <DrawerFooter>
+        <DrawerFooter className="items-start flex-row">
           <Button>Submit</Button>
           <DrawerClose>
             <Button variant="outline">Cancel</Button>
