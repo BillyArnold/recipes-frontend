d9996f2d504ab901531975d8815173c0f16121b4
Billy Arnold <billyarnoldd@gmail.com>
2024-02-06 21:40:26 +0000

    WIP adding in mealplans to draggable list

--- app/mealplans/[mealPlanId]/page.tsx
+++ app/mealplans/[mealPlanId]/page.tsx
@@ -13,7 +13,7 @@ const ListingPage = async ({ params }: { params: { mealPlanId: number } }) => {
     <div>
       <div className="text-left block w-[1100px] mx-auto max-w-full pt-4 px-8">
         <MealPlanTitleInput mealPlanId={mealPlan.id} title={mealPlan.name} />
-        <DraggableList />
+        <DraggableList mealPlan={mealPlan} />
       </div>
     </div>
   );
--- components/mealPlans/draggableList.tsx
+++ components/mealPlans/draggableList.tsx
@@ -1,20 +1,38 @@
-'use client';
+"use client";
 
-import { Reorder } from "framer-motion"
-import { useState } from "react"
+import { Reorder } from "framer-motion";
+import { useState } from "react";
 
-export default function DraggableList() {
-  const [items, setItems] = useState([0,1,2,3,4,5]);
+interface DraggableListProps {
+  mealPlan: any;
+}
+
+export default function DraggableList({ mealPlan }: DraggableListProps) {
+  const [recipes, setRecipes] = useState(mealPlan.recipes);
+
+  const getRecipeIds = (mealPlan: any) => {
+    const recipeIds: any[] = [];
+
+    mealPlan.recipes.forEach((recipe: any) => {
+      recipeIds.push(recipe.recipe.id);
+    });
+
+    return recipeIds;
+  };
+
+  const [values, setValues] = useState(getRecipeIds(mealPlan));
 
   return (
-    <Reorder.Group axis="y" values={items} onReorder={setItems}>
-      {items.map((item) => (
-        <Reorder.Item className="bg-white p-6 shadow-lg mb-6 cursor-pointer rounded-md text-black" key={item} value={item}>
-          {item}
+    <Reorder.Group axis="y" values={values} onReorder={setRecipes}>
+      {recipes.map((recipe: any) => (
+        <Reorder.Item
+          className="bg-white p-6 shadow-lg mb-6 cursor-pointer rounded-md text-black"
+          key={recipe.id}
+          value={recipe.day}
+        >
+          {recipe.recipe.name}
         </Reorder.Item>
       ))}
     </Reorder.Group>
-  )
+  );
 }
-
-
